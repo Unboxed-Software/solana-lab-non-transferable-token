809c2a370c5acc52fc6dee9b38df89514f0d9c76
AdityaKulkarni <kulkarniaditya1997@gmail.com>
2024-03-11 12:47:01 -0700

    Starter code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import {
-	Cluster,
-	Connection,
-	clusterApiUrl,
-	Keypair,
-	LAMPORTS_PER_SOL,
-} from '@solana/web3.js'
+import {Cluster, Connection, clusterApiUrl, Keypair} from '@solana/web3.js'
 import {initializeKeypair} from './keypair-helpers'
-import {createNonTransferrableMint} from './create-mint'
-import {
-	TOKEN_2022_PROGRAM_ID,
-	createAccount,
-	mintTo,
-	transferChecked,
-} from '@solana/spl-token'
 
 const CLUSTER: Cluster = 'devnet'
 
@@ -32,93 +19,13 @@ async function main() {
 		'\nmint public key: ' + mintKeypair.publicKey.toBase58() + '\n\n'
 	)
 
-	/**
-	 * Creating a non-transferrable token mint
-	 */
-	const decimals = 9
+	// CREATE MINT
 
-	await createNonTransferrableMint(
-		CLUSTER,
-		connection,
-		payer,
-		mintKeypair,
-		decimals
-	)
+	// CREATE SOURCE ACCOUNT AND MINT TOKEN
 
-	/**
-	 * Creating a source account for a transfer and minting 1 token to that account
-	 */
-	console.log('Creating a source account...')
-	const sourceKeypair = Keypair.generate()
-	const sourceAccount = await createAccount(
-		connection,
-		payer,
-		mint,
-		sourceKeypair.publicKey,
-		undefined,
-		{commitment: 'finalized'},
-		TOKEN_2022_PROGRAM_ID
-	)
+	// CREATE DESTINATION ACCOUNT FOR TRANSFER
 
-	console.log('Minting 1 token...\n\n')
-	const amount = 1 * LAMPORTS_PER_SOL
-	await mintTo(
-		connection,
-		payer,
-		mint,
-		sourceAccount,
-		payer,
-		amount,
-		[payer],
-		{commitment: 'finalized'},
-		TOKEN_2022_PROGRAM_ID
-	)
-
-	/**
-	 * Creating a destination account for a transfer
-	 */
-	console.log('Creating a destination account...\n\n')
-	const destinationKeypair = Keypair.generate()
-	const destinationAccount = await createAccount(
-		connection,
-		payer,
-		mintKeypair.publicKey,
-		destinationKeypair.publicKey,
-		undefined,
-		{commitment: 'finalized'},
-		TOKEN_2022_PROGRAM_ID
-	)
-
-	/**
-	 * Trying transferring 1 token from source account to destination account.
-	 *
-	 * Should throw `SendTransactionError`
-	 */
-	console.log('Trying transferring non-transferrable mint...')
-	try {
-		const signature = await transferChecked(
-			connection,
-			payer,
-			sourceAccount,
-			mint,
-			destinationAccount,
-			sourceAccount,
-			amount,
-			decimals,
-			[sourceKeypair, destinationKeypair],
-			{commitment: 'finalized'},
-			TOKEN_2022_PROGRAM_ID
-		)
-		console.log(
-			`Check the transaction at: https://explorer.solana.com/tx/${signature}?cluster=${CLUSTER}`
-		)
-	} catch (e) {
-		console.log(
-			'This transfer is failing because the mint is non-transferrable. Check out the program logs: ',
-			(e as any).logs,
-			'\n\n'
-		)
-	}
+	// TRY TRANSFER
 }
 
 main()
